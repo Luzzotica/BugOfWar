a58f8da67125c66df9e8f3f5b74f41741dc834bb
Eric Riddoch <eric.riddoch@bengroup.com>
2022-06-25 17:51:53 -0600

    food items now spawn in random map locations with a random delay on the BasicMap; ants can drag into anthill

--- Entities/BugOfWar/Anthill.gd
+++ Entities/BugOfWar/Anthill.gd
@@ -4,6 +4,44 @@ extends Node
 # var a = 2
 # var b = "text"
 
+var team_points: int = 0
+
+onready var anthill_area: Area2D = $"Anthill Area"
+
+
+func _physics_process(delta):
+	consume_consumables_in_anthill_area()
+
+
+func consume_consumables_in_anthill_area():
+	var consumables: Array = get_consumables_in_anthill_area()
+	for consumable in consumables:
+		consume(consumable)
+
+
+func get_consumables_in_anthill_area():
+	var physics_bodies_in_anthill_area: Array = anthill_area.get_overlapping_bodies()
+	var consumable_bodies: Array = try_get_consumable_bodies(physics_bodies_in_anthill_area)
+	return consumable_bodies
+
+
+func try_get_consumable_bodies(physics_bodies: Array) -> Array:
+	var result := Array()
+	for body in physics_bodies:
+		if is_consumable(body):
+			result.append(body)
+	return result
+
+
+func is_consumable(object: Object) -> bool:
+	return object.has_method("on_consumed")
+
+
+func consume(consumable: Object) -> void:
+	team_points += consumable.point_value
+	consumable.on_consumed()
+	print("We now have", team_points, "points!")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
---
+++ Entities/BugOfWar/FoodSpawnArea.gd
@@ -0,0 +1,72 @@
+extends Node2D
+class_name FoodSpawnArea
+
+export(int) var max_food_items = 2
+export(float) var min_wait_time_seconds = 1
+export(float) var max_wait_time_seconds = 5
+export(PackedScene) var Food
+
+onready var food_group: Node = $"FoodGroup"
+onready var spawn_area_shape: CollisionShape2D = $"SpawnArea/Shape"
+onready var spawn_area_rect: RectangleShape2D = spawn_area_shape.shape
+onready var spawn_timer: Timer = $SpawnTimer
+
+
+func _ready():
+	spawn_timer.connect("timeout", self, "_on_spawn_wait_timeout")
+	start_random_wait()
+
+
+func _on_SpawnTimer_timeout():
+	print("Wait complete!")
+	spawn_food_if_should()
+	start_random_wait()
+
+
+func spawn_food_if_should() -> void:
+	print("Time remaining ", spawn_timer.time_left, " is_wait_complete() ", is_wait_complete())
+
+	if not is_max_food_spawned():
+		spawn_food_in_random_position_in_spawn_area()
+
+
+func spawn_food_in_random_position_in_spawn_area():
+	var random_pos: Vector2 = get_random_position_in_spawn_area()
+	spawn_food(random_pos)
+
+
+func spawn_food(init_pos: Vector2) -> void:
+	print("Spawning new food at ", init_pos)
+
+	var new_food = Food.instance()
+	new_food.position = init_pos
+	new_food.point_value = 10
+
+	food_group.add_child(new_food)
+
+
+func get_current_food_count() -> int:
+	return food_group.get_child_count()
+
+
+func start_random_wait() -> void:
+	var wait_time: float = get_wait_time_seconds()
+	spawn_timer.start()
+	spawn_timer.set_wait_time(wait_time)
+	print("Starting a ", wait_time, " second wait.")
+
+
+func is_wait_complete() -> bool:
+	return spawn_timer.time_left <= 0
+
+
+func is_max_food_spawned() -> bool:
+	return get_current_food_count() >= max_food_items
+
+
+func get_wait_time_seconds() -> float:
+	return rand_range(min_wait_time_seconds, max_wait_time_seconds)
+
+func get_random_position_in_spawn_area() -> Vector2:
+	var spawn_position: Vector2 = RandomCoordinateFactory.get_point_in_rectangle(spawn_area_rect)
+	return spawn_position
